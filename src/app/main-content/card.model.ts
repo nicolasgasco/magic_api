9cdb195c5c0f76307a9cb35ca43e7c5c21d7f9b4
nicolasgasco <nicolas.gasco92@gmail.com>
2021-08-27 19:19:56 +0200

    Improving card module

--- src/app/main-content/card.model.ts
+++ src/app/main-content/card.model.ts
@@ -1,8 +1,6 @@
 export class Card {
-    public name: string;
-    public imagePath: string;
-
-    constructor(name: string, imagePath: string) {
+    // Public is to avoid the repetition of declaring and adding to constructor
+    constructor(public name: string, public imagePath: string) {
         this.name = name;
         this.imagePath = imagePath;
     }
