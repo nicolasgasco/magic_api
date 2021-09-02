716f76700151ac30f71ef36dcd1e59f5e1cea184
nicolasgasco <nicolas.gasco92@gmail.com>
2021-09-02 21:19:00 +0200

    Added class for sets

---
+++ src/app/main-content/Set.model.ts
@@ -0,0 +1,9 @@
+export class Set {
+  // Public is to avoid the repetition of declaring and adding to constructor
+  constructor(
+    public name: string,
+    public code: string,
+    public type: string,
+    public year: number
+  ) {}
+}
