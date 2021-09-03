87a699b2b738945fb4fd76455a4b03d59a54e347
nicolasgasco <nicolas.gasco92@gmail.com>
2021-09-03 17:02:49 +0200

    Styled set choice select tag

--- src/app/main-content/card-search/card-search.component.ts
+++ src/app/main-content/card-search/card-search.component.ts
@@ -1,7 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-search',
-  templateUrl: './card-search.component.html',
+  template: `
+    <section>
+      <h2>Search the name of a card below</h2>
+      <p class="magic-symbols">< = > @</p>
+      <form>
+        <input
+          type="text"
+          class="card-name-input"
+          [(ngModel)]="cardName"
+          placeholder="Insert card name..."
+        />
+      </form>
+      <small>Search only works in Italian for now :(</small>
+    </section>
+  `,
   styleUrls: ['./card-search.component.scss'],
 })
 export class CardSearchComponent implements OnInit {
