91d2e800d0b34fb088ebeecb55b964540183e73d
nicolasgasco <nicolas.gasco92@gmail.com>
2021-09-03 19:02:55 +0200

    Working on logic for name search

--- src/app/main-content/card-search/card-search.component.ts
+++ src/app/main-content/card-search/card-search.component.ts
@@ -1,27 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { not } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-search',
   template: `
     <section>
-      <h2>Search the name of a card below</h2>
-      <p class="magic-symbols">< = > @</p>
-      <form>
+      <div>
+        <h2>Search the name of a card below</h2>
+        <p class="magic-symbols">< = > @</p>
         <input
           type="text"
           class="card-name-input"
           [(ngModel)]="cardName"
           placeholder="Insert card name..."
+          (input)="onChangeName($event)"
         />
-      </form>
-      <small>Search only works in Italian for now :(</small>
+        <small>Search only works in Italian for now :(</small>
+      </div>
+      <ng-container *ngIf="namedCards.length > 0">
+        <span>CIao</span>
+        <!-- <app-card-grid [namedCards]="namedCards"></app-card-grid> -->
+      </ng-container>
+      <ng-container *ngIf="nothingFound">
+        <span>No results.</span>
+      </ng-container>
     </section>
   `,
   styleUrls: ['./card-search.component.scss'],
 })
 export class CardSearchComponent implements OnInit {
   cardName = '';
-  constructor() {
+  namedCards: [] = [];
+  typingDelay;
+  nothingFound: boolean = false;
+
+  constructor(private http: HttpClient) {
     console.log(this.cardName);
+  }
+
+  onChangeName(event) {
+    clearTimeout(this.typingDelay);
+    this.typingDelay = setTimeout(() => {
+      this.http
+        .get(
+          `https://api.magicthegathering.io/v1/cards?name=${event.target.value}&language=italian`
+        )
+        .subscribe((cards) => {
+          if (cards['cards'] && cards['cards'].length !== 0) {
+            this.namedCards = cards['cards'];
+          } else {
+            this.nothingFound = true;
+          }
+        });
+    }, 500);
   }
 
   ngOnInit(): void {}
